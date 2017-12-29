518f765ba487d7cf8326f411ffd863a37d5ef2bc
Nick Mathewson <nickm@torproject.org>
2017-12-29 12:42:40 -0500

    test the command encoders I have so far

--- src/command.rs
+++ src/command.rs
@@ -35,9 +35,9 @@ impl<'a> Command for SetConf<'a> {
     type Reply = reply::BasicReply;
     fn encode_into<W>(&self, w : &mut W) -> Result<(),io::Error>
         where W : Write + ?Sized {
-        write!(w, "SETCONF ")?;
+        write!(w, "SETCONF")?;
         for &(k,v) in self.values.iter() {
-            write!(w, "{}=", k)?;
+            write!(w, " {}=", k)?;
             write_quoted(w, v.as_bytes())?;
         }
         write!(w, "\r\n")
@@ -78,8 +78,7 @@ impl<'a> Command for GetConf<'a> {
 
 // SETEVENTS
 
-// SETEVENTS
-// AUTHENTICATE ++
+// AUTHENTICATE
 
 pub struct Authenticate<'a> {
     authdata : &'a [u8],
@@ -87,11 +86,14 @@ pub struct Authenticate<'a> {
 }
 
 impl<'a> Authenticate<'a> {
-    pub fn new_cookie(cookie: &'a [u8]) -> Self {
+    pub fn with_cookie(cookie: &'a [u8]) -> Self {
         Authenticate { authdata : cookie, is_password : false }
     }
-    pub fn new_passwd(passwd: &'a [u8]) -> Self {
+    pub fn with_passwd(passwd: &'a [u8]) -> Self {
         Authenticate { authdata : passwd, is_password : true }
+    }
+    pub fn with_nothing() -> Self {
+        Authenticate::with_cookie(b"")
     }
 }
 
@@ -155,9 +157,9 @@ impl AuthChallenge {
     pub fn new<R:rand::Rng>(rng : &mut R) -> AuthChallenge {
         let mut nonce = vec![0;32];
         rng.fill_bytes(&mut nonce);
-        AuthChallenge { nonce }
+        AuthChallenge::with_nonce(&nonce)
     }
-    pub fn new_with_nonce(use_nonce : &[u8]) -> AuthChallenge {
+    pub fn with_nonce(use_nonce : &[u8]) -> AuthChallenge {
         let mut nonce = Vec::new();
         nonce.extend_from_slice(use_nonce);
         AuthChallenge { nonce }
@@ -199,8 +201,117 @@ fn write_hex<W>(w : &mut W, data:&[u8]) -> Result<(),io::Error>
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    use super::*;
+
+    // as Command::encode, but panics on any kind of failure and produces a
+    // string.
+    fn to_string<C:Command>(command : &C) -> String {
+        String::from_utf8(command.encode().unwrap()).unwrap()
     }
+
+    #[test]
+    fn test_write_hex() {
+        fn to_hex(data: &[u8]) -> Vec<u8> {
+            let mut result = Vec::new();
+            write_hex(&mut result, data).unwrap();
+            result
+        }
+        assert_eq!(b"".to_vec(), to_hex(b""));
+        assert_eq!(b"abcd".to_vec(), to_hex(b"\xab\xcd"));
+        assert_eq!(b"416e20617363696920737472696e672e".to_vec(),
+                   to_hex(b"An ascii string."));
+    }
+
+    #[test]
+    fn test_write_quoted() {
+        // XXXX
+    }
+
+    #[test]
+    fn test_setconf() {
+        let mut command = SetConf::new();
+        assert_eq!(to_string(&command),
+                   "SETCONF\r\n");
+
+        command.add("log", "info");
+        command.add("cheese", "gruyere");
+
+        assert_eq!(to_string(&command),
+                   "SETCONF log=\"info\" cheese=\"gruyere\"\r\n");
+    }
+
+    // RESETCONF
+
+    #[test]
+    fn test_getconf() {
+        let mut command = GetConf::new();
+        assert_eq!(to_string(&command),
+                   "GETCONF\r\n");
+        command.add("cheese");
+        command.add("scambledEggs");
+
+        assert_eq!(to_string(&command),
+                   "GETCONF cheese scambledEggs\r\n");
+    }
+
+    // SETEVENTS
+
+    #[test]
+    fn test_authenticate_cookie() {
+        let command = Authenticate::with_cookie(b"hello");
+        assert_eq!(to_string(&command),
+                   "AUTHENTICATE 68656c6c6f\r\n")
+    }
+    #[test]
+    fn test_authenticate_passwd() {
+        let command = Authenticate::with_passwd(b"hello world");
+        assert_eq!(to_string(&command),
+                   "AUTHENTICATE \"hello world\"\r\n")
+    }
+    #[test]
+    fn test_authenticate_empty() {
+        let command = Authenticate::with_nothing();
+        assert_eq!(to_string(&command),
+                   "AUTHENTICATE \r\n")
+    }
+
+    // SAVECONF
+    // SIGNAL
+    // MAPADDRESS
+    // GETINFO
+    // EXTENDCIRCUIT
+    // SETCIRCUITPURPOSE
+    // SETROUTERPURPOSE
+    // ATTACHSTREAM
+    // POSTDESCRIPTOR
+    // REDIRECTSTREAM
+    // CLOSESTREAM
+    // CLOSECIRCUIT
+    // QUIT
+    // USEFEATURE
+    // RESOLVE
+
+    #[test]
+    fn test_protocolinfo() {
+        assert_eq!(to_string(&ProtocolInfo::new()),
+                   "PROTOCOLINFO 1\r\n");
+    }
+
+    // LOADCONF
+    // TAKEOWNERSHIP
+
+    #[test]
+    fn test_authchallenge_nonce() {
+        let command = AuthChallenge::with_nonce(b"arbitrary_string");
+        assert_eq!(to_string(&command),
+                   "AUTHCHALLENGE 6172626974726172795f737472696e67\r\n");
+    }
+    // XXXX need test for random case.
+
+    // DROPGUARDS
+    // HSFETCH
+    // ADD_ONION
+    // DEL_ONION
+    // HSPOST
+
 }
